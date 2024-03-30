5fb782a703a4768a566e91a006e12b3390bd2cb4
ontaras <ontaras@yahoo.com>
2024-03-30 19:54:57 -0400

    js contuct us

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,15 +1,27 @@
 const submitBtn = document.getElementById('submitBtn');
-const answer = document.getElementById('answer');
 
-function thankyou() {
+
+function thankyou(event) {
+    event.preventDefault();
+
     const name = document.getElementById('name').value.toUpperCase();
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
+    
     if (name && email && message) {
-        answer.innerHTML = `get in touch, ${name}`;
+        document.getElementById('answer').innerHTML = `<p>THANKS, ${name},</p> 
+        <p>WE'll GET IN TOUCH!</p>`;
     } else {
         window.alert("Input correct values");
     }
+   
+    reset();
+}
+
+function reset() {
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
 }
 
 submitBtn.addEventListener('click', thankyou);
