1e2083ef38f620435db5da65f354a8b6b5907faa
ontaras <ontaras@yahoo.com>
2024-04-02 09:11:30 -0400

    logic

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -12,15 +12,44 @@ function searchCondition(event) {
         //const country = data.countries.find(item => item.name.toLowerCase() === input);
 
         if (input === 'australia' || input === 'brazil' || input === 'japan') {
+
             const country = data.countries.find(item => item.name.toLowerCase() === input);
-            resultDiv.innerHTML += `<fieldset><img src="${country['cities'][0]['imageUrl']}" style='width:500px; height:250px;'><p>${country['cities'][0]['name']}<p>${country['cities'][0]['description']}</fieldset>`;
-            resultDiv.innerHTML += `<fieldset><img src="${country['cities'][1]['imageUrl']}" style='width:500px; height:250px;'><p>${country['cities'][1]['name']}<p>${country['cities'][1]['description']}</fieldset>`;
+            //debugger;
+            
+            for (i in country) {
+                console.log(country[i]);
+                for (j in country[i]) {
+                    console.log(country[i]['id'] + country[i]['name'] + country[i][j]['name']);
+                }
+            }
+            
+        
+                
+            
+
+            // for (i in country) {
+            //     console.log(country[i]);
+            //     for (j in country[i]) {
+            //         console.log(country[i]['name'] + country[i]['cities']);
+            //         for (k in country[i][j]) {
+            //             console.log(country[i][j]['name'] + ', ' + country[i][j]['description']+ ', ' + country[i][j]['imageUrl']);
+            //             //resultDiv.innerHTML += `<fieldset>${country[i][j]['name']}<p>${country[i][j]['description']}<p>${country[i][j]['imageUrl']}</fieldset>`;
+            //             break;
+            //         }
+
+            //     }
+            // }
+            // resultDiv.innerHTML += `<fieldset><img src="${country['cities'][0]['imageUrl']}" style='width:500px; height:250px;'><p>${country['cities'][0]['name']}<p>${country['cities'][0]['description']}</fieldset>`;
+            // resultDiv.innerHTML += `<fieldset><img src="${country['cities'][1]['imageUrl']}" style='width:500px; height:250px;'><p>${country['cities'][1]['name']}<p>${country['cities'][1]['description']}</fieldset>`;
+
         } else if (input === 'beach' || input === 'beaches') {
             const beaches = data.beaches;
             for (i in beaches) {
                 console.log(beaches[i]);
                 for (n in beaches[i]) {
                     console.log(beaches[i]['name'] + ', ' + beaches[i]['description']);
+                    resultDiv.innerHTML += `<fieldset>${beaches[i]['name']}</fieldset>`;
+                    break;
                 }
             }
             
